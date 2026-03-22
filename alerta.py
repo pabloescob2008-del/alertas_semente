9d0a5744387325eb2afac1ca63b44fbd3987df7a
pabloescob2008-del <pabloescob2008@gmail.com>
2026-03-22 17:31:44 -0300

    Update alerta.py

--- alerta.py
+++ alerta.py
@@ -1,14 +1,28 @@
 import requests
-import os
 
-TOKEN = os.getenv("8757944104:AAGDGKY3TlDy_hc2sR-na1e1LTEEoIIiq-w")
-CHAT_ID = os.getenv("8705265153")
+TOKEN = "8757944104:AAGDGKY3TlDy_hc2sR-na1e1LTEEoIIiq-w"
+CHAT_ID = "8705265153"
 
-def enviar_alerta(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "text": msg
-    })
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-enviar_alerta("🚀 Teste rodando no GitHub Actions!")
+response = requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": "🚀 TESTE DIRETO!"
+})
+
+print(response.text)
+
+#import requests
+#import os
+
+#TOKEN = os.getenv("8757944104:AAGDGKY3TlDy_hc2sR-na1e1LTEEoIIiq-w")
+#CHAT_ID = os.getenv("8705265153")
+
+#def enviar_alerta(msg):
+#    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+#   requests.post(url, data={
+#       "chat_id": CHAT_ID,
+#       "text": msg
+#   })
+
+#enviar_alerta("🚀 Teste rodando no GitHub Actions!")
