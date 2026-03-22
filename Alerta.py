7563e208061b9c8ed0146169248235f61f44811e
pabloescob2008-del <pabloescob2008@gmail.com>
2026-03-22 16:45:44 -0300

    Update Alerta.py

--- Alerta.py
+++ Alerta.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = os.getenv("8757944104:AAGDGKY3TlDy_hc2sR-na1e1LTEEoIIiq-w")
+CHAT_ID = os.getenv("8705265153")
 
 def enviar_alerta(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
